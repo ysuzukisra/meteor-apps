49257d2e97763ea6ebaabba37cb664045b350d6b
y.suzuki <y-suzuki@sra.co.jp>
2016-06-29 15:31:34 +0000

    up

--- qrtest/qrtest.js
+++ qrtest/qrtest.js
@@ -2,18 +2,27 @@
 
 if (Meteor.isClient) {
     Template.qrtest.onCreated(function() {
-        this.val = new ReactiveVar("test");
+        this.val = new ReactiveVar("test test !! test");
     });
-    
+    Template.qrtest.onRendered(function() {
+        console.log("OKwww");
+        $('#qrcode').empty().qrcode({text: this.val.get(), size:256});
+    });
     Template.qrtest.helpers({
-      val: function() {
-        return Template.instance().val.get();
-      }
+        val: function() {
+            return Template.instance().val.get();
+        },
+    });
+    Template.qrtest.events({
+        "keyup #qrtext": function(event) {
+            var val = $('#qrtext').val();
+            event.preventDefault();
+            //console.log("keyup! <" + val + ">");
+            Template.instance().val.set(val);
+            $('#qrcode').empty().qrcode({text: val, size:128});
+        },
     });
 }
 
 Router.myRoute('/qrtest', 'qrtest', 'QRテスト', 100, {
-   data: function() {
-      return {a: 1, b: 2};
-   },
 });
